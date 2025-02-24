18d26e8cb1481921184b246d28af80aa77a13e7d
Pranjal Verma <pvcodes@gmail.com>
2025-02-24 17:46:04 +0530

    logging and cors fix

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import fs from 'fs';
 import path from 'path';
+import cors from 'cors'; // Import the cors package
 
 import { expenseRouter, userRouter } from './routes';
 import db from './db';
@@ -9,15 +10,17 @@ import { NODE_ENV, SERVER_PORT } from './constants';
 
 const app = express();
 app.use(express.json());
+app.use(cors()); // Use the cors middleware
 
-if (NODE_ENV === 'production') {
+if (NODE_ENV === 'PRODUCTION') {
   const accessLogStream = fs.createWriteStream(
     path.join(__dirname, '../access.log'),
     { flags: 'a' }
   );
-  app.use(morgan('combined', { stream: accessLogStream }));
+  app.use(morgan('combined', { stream: accessLogStream })); // Log to file
+  app.use(morgan('combined')); // Log to console
 } else {
-  app.use(morgan('dev'));
+  app.use(morgan('dev', { stream: process.stdout })); // Log to console in dev format
 }
 
 // TODO: a universal response middleware
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
 export type TokenType = 'google-oauth' | 'basic';
-export type NODE_ENV_ENUM = 'production' | 'development'
+export type NODE_ENV_ENUM = 'PRODUCTION' | 'DEVELOPMENT';
 
 export interface GACCESS_TOKEN_RESPONSE {
   email: string;
